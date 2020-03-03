e7a92e8a1f6850207f68a3c0631f142ec6f44db8
Mafmax <m.a.f.max@mail.ru>
2020-03-03 16:39:50 +0500

    Класс ProcessedCustomer
    Реализован класс, оражающий обслуженного клиента

---
+++ CrmBl/Model/ProcessedCustomer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    public class ProcessedCustomer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Phone { get; set; }
+        public ServiceKind ServiceKindId { get; set; }
+        public decimal Cash { get; set; }
+
+        public Master MasterId { get; set; }
+
+        public DateTime Date { get; set; }
+        public ProcessedCustomer()
+        {
+
+        }
+
+    }
+}
