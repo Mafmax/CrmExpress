cf894aebca2903097f8626d2facd44301c74e88b
Mafmax <m.a.f.max@mail.ru>
2020-03-03 18:10:43 +0500

    Перечислены сущности в БД

--- CrmBl/Model/CrmContext.cs
+++ CrmBl/Model/CrmContext.cs
@@ -10,5 +10,17 @@ namespace CrmBl.Model
     class CrmContext : DbContext
     {
         public CrmContext() : base("CrmConnection") { }
+
+
+        public DbSet<Master> Masters { get; set; }
+        public DbSet<ServiceType>  ServiceTypes { get; set; }
+        public DbSet<ServiceKind> ServiceKinds { get; set; }
+        public DbSet<ProcessedCustomer> ProcessedCustomers  { get; set; }
+        public DbSet<Order> Orders  { get; set; }
+        public DbSet<Review> Reviews  { get; set; }
+
+
+
+
     }
 }
