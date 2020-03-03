e91ba82bd25ceec7f6a98f53e0c15dcfc1ec5f99
Mafmax <m.a.f.max@mail.ru>
2020-03-03 17:29:53 +0500

    Реализован класс Order
    Реализованн класс, отвечающий за данные по заявке клиента

---
+++ CrmBl/Model/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Phone { get; set; }
+        public ICollection<ServiceKind> ServiceKinds { get; set; }
+
+        public decimal AllPrice { get; set; }
+        public ICollection<Master> PreferMasters { get; set; }
+
+        // Предпочитаемая дата исполнения заявки.
+        public ICollection<DateTime> PreferDates { get; set; }
+
+        // Дата оформления заявки.
+        public DateTime Date { get; set; }
+
+
+        public Order()
+        {
+            ServiceKinds = new List<ServiceKind>();
+            PreferDates = new List<DateTime>();
+            PreferMasters = new List<Master>();
+        }
+
+    }
+}
