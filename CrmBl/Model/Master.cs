93b693460fadd464d5db0f8e68efd70f3df482ce
Mafmax <m.a.f.max@mail.ru>
2020-03-03 12:38:38 +0500

    Реализованны классы Master, ServiceKind, ServiceType

--- CrmBl/Model/Master.cs
+++ CrmBl/Model/Master.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CrmBl.Model
 {
     public class Master
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+
+        public virtual ICollection<ServiceType> ServiceTypes { get; set; }
+
+        public virtual ICollection<ServiceKind> ServiceKinds { get; set; }
+        public string Description { get; set; }
+
+
+
+
+
+        public Master()
+        {
+            ServiceTypes = new List<ServiceType>();
+            ServiceKinds = new List<ServiceKind>();
+        }
     }
 }
--- CrmBl/Model/ServiceType.cs
+++ CrmBl/Model/ServiceType.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 
 namespace CrmBl.Model
 {
     public class ServiceType
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
 
+        public virtual ICollection<Master> Masters { get; set; }
+        public virtual ICollection<ServiceKind> ServiceKinds { get; set; }
+
+
+        public ServiceType()
+        {
+            Masters = new List<Master>();
+            ServiceKinds = new List<ServiceKind>();
+        }
     }
 }
